b300e54c52ea48e452db6332f7efc47461492a7c
Angelo Arcillas <angeloarcillas64@gmail.com>
2022-07-21 13:42:47 +0800

    init starter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import grapesjs from 'grapesjs';
+
 import loadComponents from './components';
 import loadBlocks from './blocks';
 
-export default (editor, opts = {}) => {
+export default grapesjs.plugins.add('zerexei-components', (editor, opts = {}) => {
   const options = { ...{
     // default options
   },  ...opts };
@@ -11,7 +13,4 @@ export default (editor, opts = {}) => {
 
   // Add blocks
   loadBlocks(editor, options);
-
-  // TODO Remove
-  editor.on('load', () => editor.addComponents(`<div style="margin:100px; padding:25px;">Content loaded from the plugin</div>`, { at: 0 }))
-};
+});
